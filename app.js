42dd7856a89624bedb00a05d6d6f2c7926eb4cde
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-12 03:07:03 +0300

    courses model and route added

--- app.js
+++ app.js
@@ -1,16 +1,34 @@
-const express = require("express");
-const pageRoute = require("./routes/pageRoute");
+const express = require('express');
+const mongoose = require('mongoose');
+const pageRoute = require('./routes/pageRoute');
+const courseRoute = require('./routes/courseRoute');
 
 const app = express();
 
+//Connect DB
+mongoose
+	.connect(`mongodb://localhost:27017/smartedu-db`, {
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+		// useFindAndModify: false,
+	})
+	.then(() => {
+		console.log('DB CONNECTED!!');
+	})
+	.catch((err) => {
+		console.log(err);
+	});
+
 //Template Engine
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 
 //Middlewares
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 //Routes
-app.use("/", pageRoute);
+app.use('/', pageRoute);
+app.use('/courses', courseRoute);
+
 
 const PORT = 3000;
 app.listen(PORT, () => {
