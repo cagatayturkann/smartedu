17b9cd2fedbf4aa6450871975fc86650bee2c613
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-14 16:32:50 +0300

    creating course feature added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,11 @@
-const User = require('../models/User');
 const bcrypt = require('bcrypt');
+const User = require('../models/User');
+const Category = require('../models/Category');
 
 exports.createUser = async (req, res) => {
 	try {
 		const user = await User.create(req.body);
-		res.status(201).json({
-			status: 'Success',
-			user,
-		});
+		res.status(201).redirect('/login');
 	} catch (error) {
 		res.status(400).json({
 			status: 'Failed',
@@ -47,8 +45,10 @@ exports.logoutUser = async (req, res) => {
 
 exports.getDashboardPage = async (req, res) => {
 	const user = await User.findOne({ _id: req.session.userId });
+	const categories = await Category.find();
 	res.status(200).render('dashboard', {
 		pageName: 'dashboard',
 		user,
+		categories
 	});
 };
