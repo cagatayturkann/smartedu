e9481a61736580b048d0fcd5735d9c87b774665f
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-15 21:40:14 +0300

    course-teacher relation added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,8 @@
 const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Category = require('../models/Category');
+const Course = require('../models/Course');
+
 
 exports.createUser = async (req, res) => {
 	try {
@@ -46,9 +48,11 @@ exports.logoutUser = async (req, res) => {
 exports.getDashboardPage = async (req, res) => {
 	const user = await User.findOne({ _id: req.session.userId });
 	const categories = await Category.find();
+	const courses = await Course.find({user:req.session.userId}) //course içindeki user id ile session içindeki user id örtüşenleri bulacak
 	res.status(200).render('dashboard', {
 		pageName: 'dashboard',
 		user,
-		categories
+		categories,
+		courses
 	});
 };
