0722061b6b041ff2ff3cdd3e81fa6f86e23123af
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-16 04:51:27 +0300

    register/login validation messages added

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,17 +2,18 @@ const bcrypt = require('bcrypt');
 const User = require('../models/User');
 const Category = require('../models/Category');
 const Course = require('../models/Course');
-
+const { validationResult } = require('express-validator');
 
 exports.createUser = async (req, res) => {
 	try {
 		const user = await User.create(req.body);
 		res.status(201).redirect('/login');
 	} catch (error) {
-		res.status(400).json({
-			status: 'Failed',
-			error,
-		});
+		const errors = validationResult(req);
+		for (let i = 0; i < errors.array().length; i++) {
+			req.flash('error', `${errors.array()[i].msg}`);
+		}
+		res.status(400).redirect('/register');
 	}
 };
 
@@ -22,10 +23,18 @@ exports.loginUser = async (req, res) => {
 		await User.findOne({ email }, (err, user) => {
 			if (user) {
 				bcrypt.compare(password, user.password, (err, same) => {
+					if (same) {
 						//USER SESSION
 						req.session.userId = user._id;
 						res.status(200).redirect('/users/dashboard');
+					} else {
+						req.flash('error', `Your password is not correct`);
+						res.status(400).redirect('/login');
+					}
 				});
+			} else {
+				req.flash('error', `User is not exist!`);
+				res.status(400).redirect('/login');
 			}
 		});
 	} catch (error) {
@@ -44,13 +53,15 @@ exports.logoutUser = async (req, res) => {
 };
 
 exports.getDashboardPage = async (req, res) => {
-	const user = await User.findOne({ _id: req.session.userId }).populate('courses');
+	const user = await User.findOne({ _id: req.session.userId }).populate(
+		'courses'
+	);
 	const categories = await Category.find();
-	const courses = await Course.find({user:req.session.userId}) //course içindeki user id ile session içindeki user id örtüşenleri bulacak
+	const courses = await Course.find({ user: req.session.userId }); //course içindeki user id ile session içindeki user id örtüşenleri bulacak
 	res.status(200).render('dashboard', {
 		pageName: 'dashboard',
 		user,
 		categories,
-		courses
+		courses,
 	});
 };
--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,10 +1,30 @@
 const express = require('express');
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
+const { body } = require('express-validator');
+const User = require('../models/User');
 
 const router = express.Router();
 
-router.route('/signup').post(authController.createUser); //route('/') refers to http://localhost:3000/users/signup
+router.route('/signup').post(
+	[
+		body('name').not().isEmpty().withMessage('Please Enter Your Name'),
+		body('email')
+			.not()
+			.isEmail()
+			.withMessage('Please Enter a Valid Email')
+			.custom((userEmail) => {
+				return User.findOne({ email: userEmail }).then((user) => {
+					if (user) {
+						return Promise.reject('Email is already exist!');
+					}
+				});
+			}),
+		body('password').not().isEmpty().withMessage('Please Enter Your Password'),
+	],
+
+	authController.createUser
+); //route('/') refers to http://localhost:3000/users/signup
 router.route('/login').post(authController.loginUser); //route('/') refers to http://localhost:3000/users/login
 router.route('/logout').get(authController.logoutUser);
 router.route('/dashboard').get(authMiddleware, authController.getDashboardPage); // http://localhost:3000/users/dashboard
