df1f8186117e80124a497f6a04f63f27e74445a0
Çağatay Türkan <cagatayturkann@gmail.com>
2022-03-16 03:04:38 +0300

    search course feature added

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -22,13 +22,27 @@ exports.createCourse = async (req, res) => {
 exports.getAllCourses = async (req, res) => {
 	try {
 		const categorySlug = req.query.categories;
+		const query = req.query.search;
 		const category = await Category.findOne({ slug: categorySlug });
 		let filter = {};
 		if (categorySlug) {
 			filter = { category: category._id };
 		}
 
-		const courses = await Course.find(filter).sort('-createdAt');
+		if (query) {
+			filter = { name: query };
+		}
+
+		if (!query && !categorySlug) {
+			(filter.name = ''), (filter.category = null);
+		}
+
+		const courses = await Course.find({
+			$or: [
+				{ name: { $regex: '.*' + filter.name + '.*', $options: 'i' } },
+				{ category: filter.category },
+			],
+		}).sort('-createdAt').populate('user');
 		const categories = await Category.find();
 		res.status(200).render('courses', {
 			courses,
@@ -49,10 +63,12 @@ exports.getCourse = async (req, res) => {
 		const course = await Course.findOne({ slug: req.params.slug }).populate(
 			'user'
 		);
+		const categories = await Category.find();
 		res.status(200).render('singleCourse', {
 			course,
 			pageName: 'courses',
-			user
+			user,
+			categories,
 		});
 	} catch (error) {
 		res.status(400).json({
